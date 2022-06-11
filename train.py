f1673ecf33a222aa46c81a747cc03d34ebd6c1d1
Qinshike <56018114+Allen-fan@users.noreply.github.com>
2022-06-11 19:19:15 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import time
 import torchvision.datasets
 from torch.utils.data import DataLoader
@@ -20,6 +21,13 @@ val_data_size = len(val_dataset)
 VGGModel = VGG16()
 VGGModel = VGGModel.cuda()
 
+weight_path = './CIFAR10_VGG16.pth'
+
+if os.path.exists(weight_path):
+    VGGModel.load_state_dict(torch.load(weight_path))
+    print('模型预加载权重成功')
+
+
 loss_fn =nn.CrossEntropyLoss()
 loss_fn = loss_fn.cuda()
 
@@ -31,45 +39,46 @@ total_train_step = 0
 total_val_step = 0
 epoch = 20
 
-
-for i in range(epoch):
-    print("----------第{}轮训练----------".format(i + 1))
-    time.sleep(0.01)
-    #训练开始
-    VGGModel.train()
-    for data in tqdm(train_dataloader):
-        imgs, targets = data
-        imgs = imgs.cuda()
-        targets = targets.cuda()
-        outputs = VGGModel(imgs)
-        loss = loss_fn(outputs, targets)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        total_train_step = total_train_step + 1
-        if total_train_step % 50 == 0:
-            print("训练次数{}，Loss：{}".format(total_train_step, loss.item()))
-
-    ExpLR.step()
-
-    VGGModel.eval()
-    total_val_loss = 0
-    total_accuracy = 0
-    with torch.no_grad():
-        for data in val_dataloader:
+with open('./loss.txt', 'a') as f:
+    for i in range(epoch):
+        print("----------第{}轮训练----------".format(i + 1))
+        time.sleep(0.01)
+        #训练开始
+        VGGModel.train()
+        for data in tqdm(train_dataloader):
             imgs, targets = data
             imgs = imgs.cuda()
             targets = targets.cuda()
             outputs = VGGModel(imgs)
             loss = loss_fn(outputs, targets)
-            total_val_loss = total_val_loss + loss
-            accuracy = (outputs.argmax(1) == targets).sum()
-            total_accuracy = total_accuracy + accuracy
 
-    print("整体测试集Loss：{}".format(total_val_loss))
-    print("整体测试集accuracy：{}".format(total_accuracy / val_data_size))
-    total_val_step = total_val_step + 1
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
+            total_train_step = total_train_step + 1
+            if total_train_step % 50 == 0:
+                print("训练次数{}，Loss：{}".format(total_train_step, loss.item()))
+
+        ExpLR.step()
+
+        VGGModel.eval()
+        total_val_loss = 0
+        total_accuracy = 0
+        with torch.no_grad():
+            for data in val_dataloader:
+                imgs, targets = data
+                imgs = imgs.cuda()
+                targets = targets.cuda()
+                outputs = VGGModel(imgs)
+                loss = loss_fn(outputs, targets)
+                total_val_loss = total_val_loss + loss
+                accuracy = (outputs.argmax(1) == targets).sum()
+                total_accuracy = total_accuracy + accuracy
+
+        print("整体测试集Loss：{}".format(total_val_loss))
+        print("整体测试集accuracy：{}".format(total_accuracy / val_data_size))
+        total_val_step = total_val_step + 1
+        f.write(f'第{i + 1}轮平均loss=={total_val_loss.item()}\n')
 torch.save(VGGModel.state_dict(), "CIFAR10_VGG16.pth")
+f.close()
